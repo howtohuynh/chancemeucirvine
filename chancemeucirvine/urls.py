92869edf007ec51ec6c7a5d29307bc5cfd3b4219
kenny <kennyhuynh2019@gmail.com>
2017-09-12 22:10:19 -0700

    URL Fix.

--- chancemeucirvine/urls.py
+++ chancemeucirvine/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', RedirectView.as_view(url='/chancemeuci/')),
-    url(r'^/', include('calculator.urls')),
+    url(r'^$', RedirectView.as_view(url='/calculator/')),
+    url(r'^calculator/', include('calculator.urls')),
     url(r'^admin/', admin.site.urls),
 ]
